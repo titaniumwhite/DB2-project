96a48824a46e0c545650efa8deb068d8f23d78fc
Gabriele De Santis <gabriele1.desantis@gmail.com>
2022-03-02 10:05:30 +0100

    Initial commit

---
+++ src/main/java/it/polimi/dbproject/HelloServlet.java
@@ -0,0 +1,37 @@
+package it.polimi.dbproject; //Replace with your own configuration
+
+import java.io.*;
+import java.sql.DriverManager;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+    private static final long serialVersionUID = 1L;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        final String DB_URL = "jdbc:mysql://localhost:3306/dbproject2022"; //Replace with your own configuration
+        final String USER = "root"; //Replace with your own configuration
+        final String PASS = "DBproject2022"; //Replace with your own configuration
+        String result = "Connection worked";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch (Exception e) {
+            result = "Connection failed"; e.printStackTrace();
+        }
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+        out.println(result);
+        out.close();
+    }
+
+    public void destroy() {
+    }
+}
+
